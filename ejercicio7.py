8745ba2b85ec0e30b8e93fa10a1189f3765ea0af
Beade <beade88@gmail.com>
2019-12-25 09:18:46 -0500

    Primeas pruebas Diccionario

---
+++ ejercicio7.py
@@ -0,0 +1,10 @@
+datos = dict()
+datos ['Nombre'] = 1
+datos ['Apellidos'] = 2
+datos ['Correo'] = 3
+datos ['Telefono'] = 4
+print datos.keys()
+print datos.values()
+print datos.items()
+print 'Nombre' in datos
+print len(datos)
