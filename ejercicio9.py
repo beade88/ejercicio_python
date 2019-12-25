9e4d4e82ed92463e7ed5eae27285d0bc2b8a94ff
Beade <beade88@gmail.com>
2019-12-25 15:33:09 -0500

    Propuesta 1 esperando por las mieles

---
+++ ejercicio9.py
@@ -0,0 +1,12 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+datos = dict()
+a={'1' : "primer", '2' : "segundo", '3' : "tercer", '4' : "cuarto"}
+print "El", a['1'], "numero es", lista[0]
+print "El", a['2'], "numero es", lista[1]
+print "El", a['3'], "numero es", lista[2]
+print "El", a['4'], "numero es", lista[3]
+print "Seguro esto lo ves como vieja escuela pq tengo que imprimir tantas veces como quiera mostrar, seguro hay un metodo para ponerlo todo en una Linea, pero seguire investigando y dame las mieles"
+print "Basicamente aqui lo que veo que te demuestro es que tengo la logica de invocar el elemento de la lista, o del diccionario que quiero y manejar el comando de print"
+print "Sigo a ver si mejoro :) :D"
+
+
