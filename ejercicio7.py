fcf92022886cbf2e4660154e94c84e7dd60d0aec
Beade <beade88@gmail.com>
2019-12-25 14:39:15 -0500

    Despues de aclar duda con Rey

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,10 +1,7 @@
 datos = dict()
-datos ['Nombre'] = 1
-datos ['Apellidos'] = 2
-datos ['Correo'] = 3
-datos ['Telefono'] = 4
-print datos.keys()
-print datos.values()
-print datos.items()
-print 'Nombre' in datos
-print len(datos)
+datos={'nombre' : "Lester", 'apellido' : "Beade", 'mail' : "beade88@gmail.com", 'telefono' : "58290007"}
+print datos['nombre']
+print datos['apellido']
+print datos['mail']
+print datos['telefono']
+
