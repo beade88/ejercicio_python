b8ba73845235b572a84c1b3a29d311244934d51e
Beade <beade88@gmail.com>
2019-12-25 14:58:35 -0500

    7 y 8 integrados

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,7 +1,8 @@
 datos = dict()
 datos={'nombre' : "Lester", 'apellido' : "Beade", 'mail' : "beade88@gmail.com", 'telefono' : "58290007"}
-print datos['nombre']
-print datos['apellido']
-print datos['mail']
-print datos['telefono']
+print "Su nomnbre es: ", datos['nombre']
+print "Su apellido es: ", datos['apellido']
+print "Su correo electronico es: ", datos['mail']
+print "Su telefono es: ", datos['telefono']
+print "El ejercicio 7 y el 8 se me parecen y por eso los puse en el 7, dime si me falto algo :)"
 
