ad62c093fa87634c84448a80c65b9da06878cc30
Reinaldo Bloopark <reinaldo@bloopark.de>
2019-12-29 01:20:25 -0500

    [FIX] Using a list

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,2 +1,22 @@
 for x in range(1, 11):
     print (x)
+
+#  Aqui me perdi :D y tuve q virar atras
+#  y es que el 2 y el 3 los hiciste juntos :D
+#  Solo una acotacion print(x), sobre el espacio
+#  despues del print
+
+# Ahora veo que falta el ejercicio6,
+# que debias volver a esto y hacerlo con una lista
+# Seria:
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for numero in lista:
+    print(numero)
+
+#  Lo importante aqui es que veas lo comodo que es iterar
+#  sobre una lista, pq ya numero toma por defecto lo que hay en la
+#  posicion. Recuerdas que en C++ habria qie hacer algo asi como
+#  for (i = 0, i < 10; i++)
+#    print(lista[i])
+# aqui es mas simple verdad???
