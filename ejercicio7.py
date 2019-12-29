ac4bb5cf8812bc1e37ca5dced02aede36ac5bef2
Beade <beade88@gmail.com>
2019-12-29 08:56:13 -0500

    With the exercise 8 description included. The validation if the phone is my cell phone or my other phone. If you change the phone value in the dictionary, the software return if it is my cell or the other phone..  :D

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,10 +1,16 @@
-datos = dict()
-datos={'nombre' : "Lester", 'apellido' : "Beade", 'mail' : "beade88@gmail.com", 'telefono' : "58290007"}
-print "Su nomnbre es: ", datos['nombre']
-print "Su apellido es: ", datos['apellido']
-print "Su correo electronico es: ", datos['mail']
-print "Su telefono es: ", datos['telefono']
-print "El ejercicio 7 y el 8 se me parecen y por eso los puse en el 7, dime si me falto algo :)"
+datos = {'nombre': "Lester", 
+	 'apellido': "Beade", 
+	 'mail': "beade88@gmail.com", 
+	 'telefono': "48709613"
+}
+print ('Su nomnbre es: ', datos['nombre'])
+print ('Su apellido es: ', datos['apellido'])
+print ('Su correo electronico es: ', datos['mail'])
+if datos['telefono'] == '58290007': 
+	print('Su telefono celular es: {}'.format(datos['telefono']))
+else: 
+	print('Su telefono fijo es: {}'.format(datos['telefono']))
+
 
 '''
 Uso aqui otra forma de hacer comantarios, cuando son muy largos se hacen asi :D
