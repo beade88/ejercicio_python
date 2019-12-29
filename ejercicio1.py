42766f3a4f09484a461d55470838bc18c15c5f3e
Reinaldo Bloopark <reinaldo@bloopark.de>
2019-12-28 23:27:36 -0500

    Notas sobre el Ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,19 @@
 print "Hola Mundo"
 print "Pichy este es mi primer ejercicio con Python"
 print "Es tarde voy a parquear :)"
+
+#  Como te habia comentado en python3 print pasa a ser una
+#  funcion y deja de ser una 'no se como rayos decirle' :D
+#  entonces lo correcto seria (aunque como lo tienes funciona)
+print('Hola mundo')
+print('Pichy este es mi primer ejercicio con Python')
+print('Es tarde voy a parquear :)')
+
+#  Otra nota importante es respecto a las cadenas de texto
+#  Puedes decir "Hola mundo" o 'Hola mundo', es decir usando
+#  comillas dobles o comillas simples. No existe un convenio
+#  al respecto, pero la mayoria de la gente (y a mi me gusta mas
+#  por lo tanto lo uso son las comillas simples)
+#  Con lo cual todas las cadenas de texto las declaro
+#  'esto es una cadena de texto'.
+#  Si ves algo serio que diga lo contrario me lo compartes porfa.
